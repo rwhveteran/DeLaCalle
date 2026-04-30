174af4bb7a0aecbbaf90e01f8bb233861fef46fc
Robert Hendrix <rwhveteran@gmail.com>
2026-04-30 00:18:42 -0700

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px" }}>
-      <h1>De la Calle al Negocio</h1>
-      <p>From the Street to Business</p>
-    </main>
-  );
-}
+
